47097803af476a5e2f873a0f90aadee703e15016
suraj-shivu <surajshivu2000@gmail.com>
2019-02-25 23:16:34 +0530

    Add files via upload

--- maps.c
+++ maps.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include"maps.h"
+
 Player map1(Player mod)
 {   printf("You have woken up in a strange place. You dont seem to recollect where you are.\n");
     printf("There seems to be some kind of alien nearby\n");
@@ -7,3 +8,47 @@ Player map1(Player mod)
     return(mod);
 
 }
+// Functions for enemy mechanics
+bot spawn(float rate)           //rate is based on random function mechanics to be defined later.
+{   bot king;
+    king.bossname="Meliodas";                           //final boss map 1 will have rate 1.
+    king.health=200*rate;                               //boss has been defined for map 1.
+    king.lifebars=4;                                    //Minions of boss have rate less than 1.
+    king.level=15*rate;
+    king.race="Demon";
+    king.mana=150*rate;
+    king.strength=100*rate;
+    king.j=0;
+    return(king);
+}
+
+bot damage(Player hero,bot king)
+{
+    printf("Enemy has been hit");
+    if(hero.j==0)                                               //Basic attacks for hero.
+    {      king.health-=hero.strength;
+           printf("Enemy has lost %3d health",hero.strength);
+    }
+    else
+    {     king.health-=hero.mana;
+           printf("Enemy has lost %3d health",hero.mana);
+    }
+
+    return(king);
+}
+
+Player attack(Player hero,bot king)
+{                                                               //basic attack by bot.
+    printf("hero has been hit");
+    if(king.j==0)
+    {      hero.health-=king.strength;
+           printf("Enemy has lost %3.f health",king.strength);
+    }
+    else
+    {     hero.health-=king.mana;
+           printf("Enemy has lost %3.f health",king.mana);
+    }
+
+    return(hero);
+}
+
--- maps.h
+++ maps.h
@@ -1,13 +1,23 @@
 typedef struct items{
     int sword,bow,arrows,food,potion;
 
-}inventory;
+}bag;
 typedef struct Players
 {
     char name[20],race[8];
-    int level,xp,mana,stamina,health;      //Defining player stats.
-    inventory one;
+    int level,xp,mana,health,strength,j;      //Defining player stats.
+    bag one;                                          // j is a variable used to differ between normal and magic attacks.
 
 } Player;
 
 Player map1(Player);
+
+typedef struct enemy{
+    float health,level,lifebars,mana,strength;
+    int j;            // j is a variable used to differ between normal and magic attacks.
+    char *bossname,*race;
+}bot;
+
+bot spawn(float rate);
+bot damage(Player hero,bot king);
+Player attack(Player hero,bot king);
